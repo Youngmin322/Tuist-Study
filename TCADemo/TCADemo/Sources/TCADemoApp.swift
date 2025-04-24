3ef1c551cf25719dad1dc416106f9e4e25c84555
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-24 10:43:46 +0900

    ContentView 및 TCADemoApp 수정: 카운터 기능 구현 및 HTTP 연결 보안 설정 추가

--- TCADemo/TCADemo/Sources/TCADemoApp.swift
+++ TCADemo/TCADemo/Sources/TCADemoApp.swift
@@ -1,10 +1,16 @@
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCADemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(initialState: CounterFeature.State()) {
+                    CounterFeature()
+                        .dependency(\.numberFact, .liveValue)
+                }
+            )
         }
     }
 }
