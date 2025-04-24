b9a39854c95976f02b361e86013ed010c4418490
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-24 13:07:39 +0900

    카운터 기능 구현: ContentView에서 카운터 값 표시 및 버튼 추가, TCADemoApp에서 ContentView에 스토어 전달

--- TCADemo/TCADemo/Sources/TCADemoApp.swift
+++ TCADemo/TCADemo/Sources/TCADemoApp.swift
@@ -10,9 +10,13 @@ import SwiftUI
 
 @main
 struct TCADemoApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: TCADemoApp.store)
         }
     }
 }
