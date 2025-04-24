e5a31c376fc5b02e978915df4ca176bd3c8af215
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-24 14:46:53 +0900

    테스트 코드 추가

--- TCADemo/TCADemo/Tests/TCADemoTests.swift
+++ TCADemo/TCADemo/Tests/TCADemoTests.swift
@@ -1,8 +1,21 @@
-import Foundation
-import XCTest
+import ComposableArchitecture
+import Testing
 
-final class TCADemoTests: XCTestCase {
-    func test_twoPlusTwo_isFour() {
-        XCTAssertEqual(2+2, 4)
+@testable import TCADemo
+
+@MainActor
+struct CounterFeatureTests {
+    @Test
+    func basics() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        await store.send(.incrementButtonTapped) { state in
+            state.count = 1
+        }
+        await store.send(.decrementButtonTapped) {
+            $0.count = 0
+        }
     }
 }
